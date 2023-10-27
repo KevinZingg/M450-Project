f45f7a4e1f441c83a00429fb08a5fed0f43da92d
kevin.zingg <kevin.zingg@siemens.com>
2023-10-27 15:29:01 +0200

    tests commit

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q6b6clv0**h9!f6=&i2imuqk6_df(im#mn2ivqlv(p-%)wfob=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kevizi.pythonanywhere.com']
+ALLOWED_HOSTS = ['kevizi.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
